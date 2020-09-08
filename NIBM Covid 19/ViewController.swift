054d0b19527c039026cbd7698f207b72246d3e70
Buddhimal <bnawgunasekara@gmail.com>
2020-09-08 21:19:24 +0530

    Init Project

---
+++ NIBM Covid 19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NIBM Covid 19
+//
+//  Created by Buddhimal Gunasekara on 9/8/20.
+//  Copyright © 2020 nibm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
