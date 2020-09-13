0111729fedbf8e4483acf0b8fbece8ea7a77d1ff
Buddhimal Gunasekara <bnawgunasekara@gmail.com>
2020-09-14 02:00:14 +0530

    add location handler

---
+++ NIBM Covid 19/Service/LocationHandler.swift
@@ -0,0 +1,29 @@
+//
+//  LocationHandler.swift
+//  NIBM Covid19
+//
+//  Created by Buddhimal Gunasekara on 9/10/20.
+//  Copyright © 2020 NIBM. All rights reserved.
+//
+
+import CoreLocation
+
+class LocationHandler: NSObject, CLLocationManagerDelegate {
+    static let shared = LocationHandler()
+    var locationManager: CLLocationManager!
+    var location: CLLocation?
+    
+    override init() {
+        super.init()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestAlwaysAuthorization()
+        }
+    }
+    
+}
